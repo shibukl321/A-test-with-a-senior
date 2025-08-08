2220657c57274ff60889d052c1750b1ddb7244cb
shibukl321 <io0907op@gmail.com>
2025-08-08 18:34:37 +0900

    Add files via upload

---
+++ game.js
@@ -0,0 +1,5 @@
+// Placeholder game initialization
+window.onload = function() {
+  const container = document.getElementById('game-container');
+  container.innerHTML = '<h1>StellaLive Fan Game</h1><p>Game will load here.</p>';
+};
